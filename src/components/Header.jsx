9ede92c496328fffb3eb7fc3de81fd96a8de8cd2
Angélica Herrera <anggelicahrr@gmail.com>
2021-03-19 16:34:25 -0500

    Se agregaron algunas carpetas para la estructura básica del proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Assets
+import '../assets/styles/components/Header.scss';
+
+function Header() {
+    return (
+        <header className="header">
+            <h5>Hello World!</h5>
+        </header>
+    );
+  }
+  
+  export default Header;
---
+++ src/containers/App.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+// Components
+import Header from '../components/Header';
+
+// Assets
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
