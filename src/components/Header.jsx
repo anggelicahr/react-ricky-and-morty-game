6e676bbb5d66a83086619b01b1e5e571a700c8e1
Angélica Herrera <anggelicahrr@gmail.com>
2021-03-19 20:53:55 -0500

    Cambio del titulo del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     return (
         <nav className="header">
             <div className="nav-wrapper">
-                <a href="#!" className="brand-logo">Logo</a>
+                <a href="#!" className="brand-logo center">Ricky & Morty</a>
             </div>
         </nav>
     );
